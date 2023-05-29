c68914f414f10523a04ac13168508c31534cb431
Pedro <ponsvpedro@gmail.com>
2023-05-29 11:35:55 +0200

    Solved small rebase issues

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import PyQt6 as qt
-
-# from PyQt6 import QtCore, QtWidgets
 from pyqtgraph import PlotWidget, plot
 import sys  # We need sys so that we can pass argv to QApplication
 from main_window import MainWindow
