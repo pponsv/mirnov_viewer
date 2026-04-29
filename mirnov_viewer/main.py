17eaa2afef398999a3d8a234af0ae23ff02a3aed
Pedro <ponsvpedro@gmail.com>
2026-04-29 22:18:24 +0200

    Add ctrl+c to close

--- mirnov_viewer/main.py
+++ mirnov_viewer/main.py
@@ -1,8 +1,12 @@
+import signal
 import sys  # We need sys so that we can pass argv to QApplication
 
 from PySide6.QtWidgets import QApplication
 
 from .main_window import MainWindow
+
+# Makes it possible to close the window with Ctrl+C in the terminal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 
 def main():
